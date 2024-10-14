3dde46b118688aed3844cc7777443e10fffdd7a5
ARTfun00 <artyrkapochinok@gmail.com>
2024-10-14 13:49:40 +0200

    mongo client configuration was finished

--- src/databases/Mongo/index.js
+++ src/databases/Mongo/index.js
@@ -1,6 +1,18 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const uri = "mongodb+srv://<db_username>:<db_password>@testmongoclaster.rzwav.mongodb.net/?retryWrites=true&w=majority&appName=TestMongoClaster";
+const {
+    MONGO_DB_USER,
+    MONGO_DB_PASSWORD,
+    MONGO_DB_CLUSTER_NAME,
+    MONGO_DB_APP_NAME,
+    ENVIRONMENT
+} = process.env
+
+// TODO: rewrite this file into the class-approach (OOP)
+const uri = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@${MONGO_DB_CLUSTER_NAME}/?retryWrites=true&w=majority&appName=${MONGO_DB_APP_NAME}`;
+if (ENVIRONMENT === 'DEV') {
+    console.log(uri);
+}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -11,16 +23,26 @@ const client = new MongoClient(uri, {
     }
 });
 
-async function run() {
+async function init() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
+    } catch {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        await close();
     }
 }
-run().catch(console.dir);
+
+async function close() {
+    try {
+        await client.close();
+    } catch (error) {
+        console.error("could not close MongoDB connection");
+    }
+}
+
+export { init, close };
+export default client;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import { close, init } from "./databases/Mongo/index.js";
+
 import express from 'express';
 
 const app = express()
@@ -6,28 +8,35 @@ const port = PORT || 3000;
 
 // i'm not sure about the trading strategy, as it affect the implementation (pooling, websockets etc. )
 // in this case i suppose the day-time range limitation could be used for the system to test-out approach
+(async function () {
+    // app.use("/trades") // this is how the routes should be connected to the app
+    // due to the limited time, i would use "brute-force"-like approach and refactor the code if the time will left at the end
+    app.get('/', async function (req, res, next) {
+        res.send('Hello World');
+    })
 
-// app.use("/trades") // this is how the routes should be connected to the app
-// due to the limited time, i would use "brute-force"-like approach and refactor the code if the time will left at the end
-app.get('/', async function (req, res, next) {
-    res.send('Hello World');
-})
+    // opening connection to the MongoDB cluster
+    await init();
 
-// server initialization status
-app.listen(port, () => {
-    console.log(`Server is listening on: http://localhost:${port}`);
-})
+    // server initialization status
+    app.listen(port, () => {
+        console.log(`Server is listening on: http://localhost:${port}`);
+    })
 
-// default error handling mechanism (for all endpoints)
-app.use((err, req, res, next) => {
-    console.error(err.stack)
+    // default error handling mechanism (for all endpoints)
+    app.use((err, req, res, next) => {
+        console.error(err.stack)
 
-    res.status(500).send('Sorry, the server is temporary unavailable');
-})
+        res.status(500).send('Sorry, the server is temporary unavailable');
+    })
+})()
 
 // handler of unhandledRejection or system interruptions of sever
 // TODO: potentially other logging mechanism could be used here
-process.on('unhandledRejection', error => {
+process.on('unhandledRejection', async (error) => {
     console.error('server unhandledRejection:', error?.message);
     console.error(error);
+
+    // closing connection with MongoDB cluster
+    await close();
 }); 
